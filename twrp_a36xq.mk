e4eb97d2bb314fec9c080b6cc48ce571589262f1
rasgaxota572 <rasgaxota575@gmail.com>
2025-07-30 09:28:03 -0300

    Update and rename omni_a36xq.mk to twrp_a36xq.mk

--- twrp_a36xq.mk
+++ twrp_a36xq.mk
@@ -10,13 +10,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from a36xq device
 $(call inherit-product, device/samsung/a36xq/device.mk)
 
 PRODUCT_DEVICE := a36xq
-PRODUCT_NAME := omni_a36xq
+PRODUCT_NAME := twrp_a36xq
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-A366E
 PRODUCT_MANUFACTURER := samsung
