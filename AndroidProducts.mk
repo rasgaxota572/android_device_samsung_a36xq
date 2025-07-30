63f1c36a660407cedd0e6ee9a2eb312aff82cc96
Sebastiano Barezzi <seba@sebaubuntu.dev>
2025-07-30 14:22:13 +0200

    a36xq: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: samsung Model: SM-A366E Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_a36xq.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_a36xq-user \
+    omni_a36xq-userdebug \
+    omni_a36xq-eng
---
+++ omni_a36xq.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from a36xq device
+$(call inherit-product, device/samsung/a36xq/device.mk)
+
+PRODUCT_DEVICE := a36xq
+PRODUCT_NAME := omni_a36xq
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-A366E
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="a36xqnsxx-user 15 AP3A.240905.015.A2 A366EXXS4AYF9 test-keys"
+
+BUILD_FINGERPRINT := samsung/a36xqnsxx/a36xq:15/AP3A.240905.015.A2/A366EXXS4AYF9:user/test-keys
