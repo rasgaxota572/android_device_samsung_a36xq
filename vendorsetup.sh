6fde6fc189347dc9121e162d89700599327f444f
rasgaxota572 <rasgaxota575@gmail.com>
2025-07-30 09:28:40 -0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_a36xq-user
-add_lunch_combo omni_a36xq-userdebug
-add_lunch_combo omni_a36xq-eng
+add_lunch_combo twrp_a36xq-user
+add_lunch_combo twrp_a36xq-userdebug
+add_lunch_combo twrp_a36xq-eng
