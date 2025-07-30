88fb7f19797cb2ed5e674cdab54c4d91cbe70216
rasgaxota572 <rasgaxota575@gmail.com>
2025-07-30 09:30:48 -0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_a36xq.mk
+    $(LOCAL_DIR)/twrp_a36xq.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_a36xq-user \
-    omni_a36xq-userdebug \
-    omni_a36xq-eng
+    twrp_a36xq-user \
+    twrp_a36xq-userdebug \
+    twrp_a36xq-eng
